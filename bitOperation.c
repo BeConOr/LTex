dc9f179ddaa96d5a3444b4917277e13c9eab1227
BeConOr <selig2014@yandex.ru>
2022-03-22 18:11:27 +0700

    'Fixed'

--- bitOperation.c
+++ bitOperation.c
@@ -3,7 +3,7 @@
 int main(void){
 	int a = 0x00CCAABB;
 	for(int i = 0; i < 4; ++i){
-		printf("Byte #%d of a: %x\n", i+1, ((a >> i*8) & 0x000000FF));
+		printf("Byte #%d of a: %x\n", i+1, ((a >> (i << 3)) & 0x000000FF));
 	}
 	a = (a & 0xFF00FFFF) | 0x00DD0000;
 	printf("New a: 0x%x\n", a);
