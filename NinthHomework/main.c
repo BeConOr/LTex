f266642c9189333788db108ff47a941a28257308
BeConOr <selig2014@yandex.ru>
2022-06-24 10:58:32 +0700

    Add dir reading

--- NinthHomework/main.c
+++ NinthHomework/main.c
@@ -10,11 +10,9 @@ int main(int argc, char ** argv)
     }
 
     WINDOW * wnd;
-	struct windows_list * wnd_list = (struct windows_list*) malloc(sizeof(struct windows_list));
-    wnd_list->next = (struct windows_list*) malloc(sizeof(struct windows_list));
-    wnd_list->previous = wnd_list->next;
-    wnd_list->next->next = wnd_list;
-    wnd_list->next->previous = wnd_list->next->next;
+	WINDOW * current_wnd;
+    WINDOW * left_wnd;
+    WINDOW * right_wnd;
 
 	initscr();
 	signal(SIGWINCH, sig_winch);
@@ -29,16 +27,67 @@ int main(int argc, char ** argv)
 
     int commander_area_width = (COLS - 2)/2;
 
-    wnd_list->value = derwin(wnd, LINES - 2, commander_area_width, 1, 1);
-    wnd_list->next->value = derwin(wnd, LINES - 2, commander_area_width, 1, commander_area_width);
+    left_wnd = derwin(wnd, LINES - 2, commander_area_width, 1, 1);
+    right_wnd = derwin(wnd, LINES - 2, commander_area_width, 1, commander_area_width);
+
+    current_wnd = left_wnd;
+
+    box(left_wnd, 0, 0);
+    box(right_wnd, 0, 0);
 
     wrefresh(wnd);
 
-	delwin(wnd_list->value);
-	delwin(wnd_list->next->value);
+//    DIR * root_dir = opendir(argv[1]);
+//    if (!root_dir) {
+//        perror("diropen");
+//        exit(1);
+//    };
 
-    free(wnd_list->next);
-    free(wnd_list);
+    wmove(left_wnd, 1, 1);
+    int dir_number = 0;
+
+    struct dirent ** directory_list;
+    dir_number = scandir(argv[1], &directory_list, 0, alphasort);
+
+    if (dir_number < 0) perror("scandir");
+
+    for (int i = 0; i < dir_number; ++i) {
+        char * dir_type;
+        switch (directory_list[i]->d_type) {
+            case DT_DIR:
+                dir_type = "directory";
+                break;
+            case DT_CHR:
+                dir_type = "text";
+                break;
+            default:
+                dir_type = "unknown";
+                break;
+        }
+        int x, y;
+        getyx(left_wnd, y, x);
+        wprintw(left_wnd, "%s [%s] %d",
+                directory_list[i]->d_name, dir_type, directory_list[i]->d_reclen);
+
+        wmove(left_wnd, y, commander_area_width/3);
+
+        wprintw(left_wnd, "%s [%s] %d",
+                directory_list[i]->d_name, dir_type, directory_list[i]->d_reclen);
+
+        wmove(left_wnd, y + 1, 1);
+    };
+
+    wrefresh(left_wnd);
+    getch();
+//    closedir(root_dir);
+
+	delwin(left_wnd);
+	delwin(right_wnd);
+
+    while(dir_number--) {
+        free(directory_list[dir_number]);
+    }
+    free(directory_list);
 
 	delwin(wnd);
 	endwin();
